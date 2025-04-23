844561e1cf04588374b6a6b4ff4707de8a97c78a
unknown <cauanhakk@gmail.com>
2025-04-23 14:00:09 -0300

    live chat

--- controllers/Webhook.js
+++ controllers/Webhook.js
@@ -1,56 +1,59 @@
 const express = require('express');
 const { Chat } = require('../entities/Chat');
-const app = express();
 const { v4: uuidv4 } = require('uuid');
 const { createChat, getChatService } = require('../services/ChatService');
 const { saveMessage } = require('../services/MessageService');
 const { Message } = require('../entities/Message');
 
+const app = express();
 app.use(express.json());
 
-app.post('/chat', async (req, res) => {
-  const result = req.body;
+module.exports = (io) => {
+  app.post('/chat', async (req, res) => {
+    const result = req.body;
 
-  try {
-    const chat = new Chat(
-      uuidv4(),
-      result.data.key.remoteJid,
-      result.data.instanceId,
-      null,
-      false,
-      null,
-      null,
-      result.data.status,
-      new Date(result.date_time).getTime(),
-      []
-    );
-
-    const createChats = await createChat(chat, 'effective_gain', result.data.message.conversation);
-    const chatDb = await getChatService(createChats, 'effective_gain');
-    await saveMessage(
-      chatDb.id,
-      new Message(
+    try {
+      const chat = new Chat(
         uuidv4(),
-        result.data.message.conversation,
-        result.data.key.fromMe,
         result.data.key.remoteJid,
-        result.data.pushName,
-        new Date(result.date_time).getTime()
-      ),
-      'effective_gain' 
-    );
+        result.data.instanceId,
+        null,
+        false,
+        null,
+        null,
+        result.data.status,
+        new Date(result.date_time).getTime(),
+        []
+      );
 
-    res.status(200).json({ result });
+      const createChats = await createChat(chat, 'effective_gain', result.data.message.conversation);
+      const chatDb = await getChatService(createChats, 'effective_gain');
+      await saveMessage(
+        chatDb.id,
+        new Message(
+          uuidv4(),
+          result.data.message.conversation,
+          result.data.key.fromMe,
+          result.data.key.remoteJid,
+          result.data.pushName,
+          new Date(result.date_time).getTime()
+        ),
+        'effective_gain'
+      );
 
-  } catch (err) {
-    console.error("Erro no webhook /chat:", err);
-    res.status(500).json({ error: err.message });
-  }
-});
+      io.emit("message", {
+        chatId: chatDb.id,
+        body: result.data.message.conversation,
+        from: result.data.pushName,
+        timestamp: new Date(result.date_time).getTime()
+      });
 
+      res.status(200).json({ result });
+    } catch (err) {
+      console.error("Erro no webhook /chat:", err);
+      res.status(500).json({ error: err.message });
+    }
+  });
 
-
-const PORT = 3002;
-app.listen(PORT, () => {
-  console.log(`Webhook ouvindo na porta ${PORT}`);
-});
+  return app;
+};
