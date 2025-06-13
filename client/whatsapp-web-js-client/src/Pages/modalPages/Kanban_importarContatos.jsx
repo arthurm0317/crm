68bd9872acacf7acec0b782dcf7789fabe80f00f
Arthur <arthurmotarm@outlook.com>
2025-06-13 12:47:43 -0300

    mensagem de disparo aparecendo

--- client/whatsapp-web-js-client/src/Pages/modalPages/Kanban_importarContatos.jsx
+++ client/whatsapp-web-js-client/src/Pages/modalPages/Kanban_importarContatos.jsx
@@ -11,7 +11,7 @@ function ImportarContatosModal({ theme, show, onHide, funil, etapas = [] }) {
   const [requiredFields, setRequiredFields] = useState({
     phone: '',
     name: '',
-    customId: '' // Campo personalizado que pode servir como ID para disparos
+    customId: ''
   });
   const [errorMsg, setErrorMsg] = useState('');
   const fileInputRef = useRef(null);
@@ -93,8 +93,9 @@ function ImportarContatosModal({ theme, show, onHide, funil, etapas = [] }) {
       const formData = new FormData();
       formData.append('file', file);
       formData.append('mapping', JSON.stringify(mapping));
+      formData.append('connection_id',)
+      formData.append('sector', funil);
       formData.append('schema', schema);
-      formData.append('funil', funil);
 
       const response = await axios.post(`${url}/kanban/import-contacts`, formData, {
         headers: {
